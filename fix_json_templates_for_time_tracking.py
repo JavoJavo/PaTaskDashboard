45fc61ab6f17324914f1d607574db2a9d5d81b2e
Javier Navarro <javojavojavojavo@gmail.com>
2026-01-20 18:23:49 -0600

    made it so that it reads files paths from  file_path_conf.yaml for no manual input.

--- fix_json_templates_for_time_tracking.py
+++ fix_json_templates_for_time_tracking.py
@@ -1,5 +1,6 @@
-import sys
-import json
+import sys, os
+import json, yaml
+from pathlib import Path
 
 def modify_tasks_for_time_tracking(task_data):
     """Recursive"""
@@ -17,6 +18,59 @@ def modify_tasks_for_time_tracking(task_data):
             step["time_tracking"] = {"start_end":[]}
 
 
+    
+def get_PATH():
+    #EXE_DIR = os.path.dirname(sys.executable)
+    #PATH = os.path.join(EXE_DIR, path)
+    if getattr(sys, "frozen", False):
+        return os.path.dirname(sys.executable)+'/'
+    return os.path.dirname(os.path.abspath(__file__))+'/'
+
+def load_file_path_conf():
+    with open(get_PATH()+'file_path_conf.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+        return config
+config = load_file_path_conf()
+
+
+
+single_file_processes_dir = get_PATH() + config["paths"]["input"]["single_file_processes_dir"]
+json_file = None
+
+for filename in os.listdir(single_file_processes_dir):
+    filepath = os.path.join(single_file_processes_dir, filename)
+    # Check if the path points to a file (and not a directory)
+    if os.path.isfile(filepath):
+        print("Updating "+filepath)
+        with open(filepath, "r") as f:
+            json_file = json.load(f)
+            json_file = json_file[0]
+            json_file["global_time_pointer"] = "None"
+            modify_tasks_for_time_tracking(json_file)
+        with open(filepath, "w") as f:
+            json_file = [json_file]
+            json.dump(json_file, f, indent=4)
+        print("Updating "+filepath+" COMPLETED.")
+
+
+template_file = get_PATH() + config["paths"]["input"]["tasks_template"]
+json_file = None
+
+print("Updating "+template_file)
+with open(template_file, "r") as f:
+    json_file = json.load(f)
+    for key, value in json_file.items():
+        json_file[key]["global_time_pointer"] = "None"
+        modify_tasks_for_time_tracking(value)
+with open(template_file, "w") as f:
+    json.dump(json_file, f, indent=4)
+print("Updating "+template_file+" COMPLETED.")
+
+
+'''
+
+is_single = False
+json_file = None
 if len(sys.argv) == 2:
     is_single = False
     json_file_path = str(sys.argv[1])
@@ -40,3 +94,4 @@ else:
           Expected 1 extra argument.
           Example: fix_json_templates_for_time_tracking.py file.json
             """)
+'''
